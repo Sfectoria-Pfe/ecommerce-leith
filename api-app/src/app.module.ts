9448e2c57ba499cac04c0958cb32ef9c2d0e18b4
khalil kraiem <khalilmedkraiem@gmail>
2024-03-03 16:22:18 +0100

    prisma seed added

--- api-app/src/app.module.ts
+++ api-app/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProgramsModule } from './programs/programs.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PrismaModule, ProgramsModule, ModulesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api-app/src/programs/programs.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProgramDto } from './dto/create-program.dto';
+import { UpdateProgramDto } from './dto/update-program.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProgramsService {
+  constructor(private readonly prisma: PrismaService) {}
+  create(createProgramDto: CreateProgramDto) {
+    return 'This action adds a new program';
+  }
+
+  findAll() {
+    return this.prisma.program.findMany({
+      include: {
+        ProgramModule: {
+          include: {
+            module: true,
+          },
+        },
+      },
+    });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} program`;
+  }
+
+  update(id: number, updateProgramDto: UpdateProgramDto) {
+    return `This action updates a #${id} program`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} program`;
+  }
+}
