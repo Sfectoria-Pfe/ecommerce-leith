cbbcfd0236b151f33c77e4217f0f681c9c5338dc
khalil kraiem <khalilmedkraiem@gmail>
2024-03-08 21:21:38 +0100

    open api swagger

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
+require("dotenv").config();
 
-require('dotenv').config()
-
-
-
-require("./model/index")
+require("./model/index");
 const path = require("path");
 const port = 3100;
 const cors = require("cors");
@@ -17,6 +16,21 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "publics")));
 
 app.use("/api/v1/products", productsRoute);
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "SFECTORIA Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(port, () => {
   console.log(`listning too`);
--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -5,10 +5,51 @@ const {
   search,
   createProduct,
   findOne,
-  deleteProduct
+  deleteProduct,
 } = require("../controller/products.controller");
-
+/**
+ * @swagger
+ * /api/v1/products/:
+ *   get:
+ *     summary: Lists all the products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: The list of the products
+ * */
 productsRoute.get("/", selectAll);
+
+/**
+ * @swagger
+ * /api/v1/products/:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type:number
+ *              productName:
+ *                type:string
+ *              imageURL:
+ *                type:string
+ *              price:
+ *                type:number
+ *              description:
+ *                type:string
+ *              category:
+ *                type:string
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *       500:
+ *         description: Some server error
+ * */
 productsRoute.post("/search", search);
 productsRoute.post("/", createProduct);
 productsRoute.get("/:id", findOne);
